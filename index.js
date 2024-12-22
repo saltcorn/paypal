87cb80ffec6c5f5800458adaaa24f1f75f4aa75b
Tom Nielsen <tanielsen@gmail.com>
2024-12-22 16:54:36 +0000

    view and action

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const {
   freeVariables,
 } = require("@saltcorn/data/models/expression");
 const { getState, features } = require("@saltcorn/data/db/state");
+const { interpolate } = require("@saltcorn/data/utils");
 
-const axios = require("axios");
-const { createHash, createHmac } = require("crypto");
 const paypal = require("paypal-rest-sdk");
 
 const onLoad = async (cfg) => {
@@ -63,10 +62,269 @@ const configuration_workflow = () => {
   });
 };
 
+const actions = () => ({
+  paypal_create_payment: {
+    configFields: async ({ table }) => {
+      const fields = table ? await table.getFields() : [];
+
+      const cbviews = await View.find({ viewtemplate: "Paypal Callback" });
+      const amount_options = fields
+        .filter((f) => ["Float", "Integer"].includes(f.type?.name))
+        .map((f) => f.name);
+      amount_options.push("Formula");
+
+      return [
+        {
+          name: "amount_field",
+          label: "Amount field",
+          type: "String",
+          required: true,
+          attributes: {
+            options: amount_options,
+          },
+        },
+        {
+          name: "amount_formula",
+          label: "Amount formula",
+          type: "String",
+          required: true,
+          fieldview: "textarea",
+          class: "validate-expression",
+          showIf: { amount_field: "Formula" },
+        },
+        {
+          name: "currency",
+          label: "Currency",
+          type: "String",
+          sublabel:
+            "3 Letter currency code e.g. USD. Use interpolations {{ }} to access fields",
+          required: true,
+        },
+        {
+          name: "callback_view",
+          label: "Callback view",
+          type: "String",
+          required: true,
+          attributes: {
+            options: cbviews.map((f) => f.name),
+          },
+        },
+      ];
+    },
+    run: async ({
+      table,
+      req,
+      user,
+      row,
+      configuration: { currency, amount_field, amount_formula, callback_view },
+    }) => {
+      let amount;
+      if (amount_field === "Formula") {
+        const joinFields = {};
+        add_free_variables_to_joinfields(
+          freeVariables(amount_formula),
+          joinFields,
+          table.fields
+        );
+        let row_eval;
+        if (Object.keys(joinFields).length > 0)
+          row_eval = (
+            await table.getJoinedRows({
+              where: { id: row.id },
+              joinFields,
+            })
+          )[0];
+        else row_eval = row;
+
+        amount = eval_expression(amount_formula, row_eval, req?.user).toFixed(
+          2
+        );
+      } else if (amount_field.includes(".")) {
+        const amt_fk_field = table.getField(amount_field);
+        const amt_table = Table.findOne(amt_fk_field.table_id);
+        const amt_row = await amt_table.getRow({
+          [amt_table.pk_name]: row[amount_field.split(".")[0]],
+        });
+        amount = amt_row[amt_fk_field.name].toFixed(2);
+      } else amount = row[amount_field].toFixed(2);
+      let use_currency = interpolate(currency, row, user);
+      const cfg_base_url = getState().getConfig("base_url");
+      const cb_url = `${cfg_base_url}view/${callback_view}?id=${row.id}&amt=${amount}&ccy=${use_currency}`;
+      // from https://www.geeksforgeeks.org/how-to-integrate-paypal-in-node/
+      const create_payment_json = {
+        intent: "sale",
+        payer: {
+          payment_method: "paypal",
+        },
+        redirect_urls: {
+          return_url: cb_url,
+          cancel_url: cb_url,
+        },
+        transactions: [
+          {
+            /*  item_list: {
+              items: [
+                {
+                  name: "Red Sox Hat",
+                  sku: "001",
+                  price: "25.00",
+                  currency: "USD",
+                  quantity: 1,
+                },
+              ],
+            },*/
+            amount: {
+              currency: use_currency,
+              total: amount,
+            },
+            description: "Hat for the best team ever",
+          },
+        ],
+      };
+
+      paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+          throw error;
+        } else {
+          for (let i = 0; i < payment.links.length; i++) {
+            if (payment.links[i].rel === "approval_url") {
+              return { json: { goto: payment.links[i].href } };
+            }
+          }
+        }
+      });
+    },
+  },
+});
+
+const viewtemplates = () => ({
+  name: "Paypal Callback",
+  display_state_form: false,
+  configuration_workflow: () =>
+    new Workflow({
+      steps: [
+        {
+          name: "Callback configuration",
+          disablePreview: true,
+          form: async (context) => {
+            const table = Table.findOne({ id: context.table_id });
+            const views = await View.find({ table_id: table.id });
+            return new Form({
+              fields: [
+                {
+                  name: "paid_field",
+                  label: "Paid field",
+                  type: "String",
+                  sublabel:
+                    "Optionally, a Boolean field that will be set to true if paid",
+                  attributes: {
+                    options: table.fields
+                      .filter((f) => f.type?.name === "Bool")
+                      .map((f) => f.name),
+                  },
+                },
+                {
+                  name: "success_view",
+                  label: "Success view",
+                  type: "String",
+                  required: true,
+                  attributes: {
+                    options: views
+                      .filter((v) => v.name !== context.viewname)
+                      .map((v) => v.name),
+                  },
+                },
+
+                {
+                  name: "failure_view",
+                  label: "Failure view",
+                  type: "String",
+                  required: true,
+                  attributes: {
+                    options: views
+                      .filter((v) => v.name !== context.viewname)
+                      .map((v) => v.name),
+                  },
+                },
+              ],
+            });
+          },
+        },
+      ],
+    }),
+  get_state_fields: () => [],
+  run: async (
+    table_id,
+    viewname,
+    {
+      reference_id_field,
+      paid_field,
+      cancelled_view,
+      success_view,
+      processing_view,
+      failure_view,
+    },
+    state,
+    { req, res }
+  ) => {
+    console.log("state", state);
+    const table = Table.findOne({ id: table_id });
+    const row = await table.getRow({
+      id: state.id,
+    });
+    const upd = {};
+
+    const payerId = state.PayerID;
+    const paymentId = state.paymentId;
+
+    const execute_payment_json = {
+      payer_id: payerId,
+      transactions: [
+        {
+          amount: {
+            currency: state.ccy,
+            total: state.amt,
+          },
+        },
+      ],
+    };
+
+    let { success, error } = await new Promise((resolve, reject) => {
+      paypal.payment.execute(
+        paymentId,
+        execute_payment_json,
+        async function (error, success) {
+          if (error) resolve({ error });
+          else resolve({ success });
+        }
+      );
+    });
+
+    let dest_url;
+    if (error) {
+      console.log("paypal error", error);
+
+      if (paid_field) upd[paid_field] = false;
+      dest_url = `/view/${success_view}?id=${row.id}`;
+    } else {
+      console.log("paypal success", JSON.stringify(success, null, 2));
+      if (paid_field) upd[paid_field] = false;
+      dest_url = `/view/${failure_view}?id=${row.id}`;
+    }
+
+    if (Object.keys(upd).length > 0)
+      await table.updateRow(upd, row[table.pk_name]);
+
+    return {
+      goto: dest_url,
+    };
+  },
+});
+
 module.exports = {
   sc_plugin_api_version: 1,
   configuration_workflow,
   onLoad,
-  // actions,
-  // viewtemplates,
+  actions,
+  viewtemplates,
 };
